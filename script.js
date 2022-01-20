d971bc6a107b2bfed677585f074c223e8837b855
Alexander Turchin <turchinsasha4@gmail.com>
2022-01-20 23:00:53 +0200

    After Dima's advice

--- script.js
+++ script.js
@@ -11,30 +11,61 @@ document.addEventListener('DOMContentLoaded', () => {
     const out = document.querySelector('.calc-screen p'); // число ввода
     const ac = document.querySelector('.ac'); // кнопка очистки
     const buttons = document.querySelector('.buttons'); // блок кнопок
+    const operationButtons = document.querySelectorAll('.division, .multiply, .minus, .plus')
 
-    const percent = (number, numberOfPercent) =>{     //функция подсчета процента
-        if (numberOfPercent === number){
-            return number/100 
+    const unpressedButtonOperation = () => {
+        operationButtons.forEach(btn => {
+            btn.style.background = '#ff9501';
+            btn.style.color = '#fff';
+        })
+    }
+
+    const pressedButtonOperation = () => {
+        operationButtons.forEach((btn, i) => {
+            btn.addEventListener('click', (e) => {
+                if (btn.style.background = '#fff') {
+                    unpressedButtonOperation()
+                }
+                btn.style.background = '#fff';
+                btn.style.color = '#ff9501';
+            })
+        })
+    }
+    pressedButtonOperation()
+
+    const percent = (number, numberOfPercent) => { //функция подсчета процента
+        if (numberOfPercent === number) {
+            return number / 100
         }
-        return (number/100) * numberOfPercent
+        return (number / 100) * numberOfPercent
     }
 
     const clearAll = () => {
         a = '';
         b = '';
-        sign = '';                      //функция очистки поля
+        sign = ''; //функция очистки поля
         finish = false;
         out.textContent = 0;
+        if (ac.textContent === 'C') {
+            ac.textContent = 'AC'
+        }
     }
 
     buttons.addEventListener('click', (event) => {
         let target = event.target; // для удобства
         // проверяем, что нажата именно кнопка, а не область
         if (target && target.classList.contains('btn')) {
+
+            if (target !== target.classList.contains('ac')) {
+                ac.textContent = 'C'
+            }
+
             // если нажата кнопка очистки, то вызываем функцию очистки
             if (target.classList.contains('ac')) {
+                unpressedButtonOperation()
                 clearAll();
                 return;
+
             }
             out.textContent = '';
             const key = target.textContent; // получение значения нажатой кнопки
@@ -44,11 +75,12 @@ document.addEventListener('DOMContentLoaded', () => {
                     a += key;
                     out.textContent = a;
                 } else if (a !== '' && b !== '' && finish) { //если нажали равно
+                    unpressedButtonOperation()
                     b = key;
                     finish = false;
                     out.textContent = b;
-                } 
-                 else {
+                } else {
+                    unpressedButtonOperation()
                     b += key;
                     out.textContent = b; // ввод нового числа после расчетов
                 }
@@ -57,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (action.includes(key)) {
                 sign = key;
-                out.textContent = sign;   //вывод знака операции
+                out.textContent = a;
                 return;
             }
             if (key === '=') {
